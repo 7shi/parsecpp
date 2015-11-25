3de69b9ef44b67895fa05fdb945cbcf7058f7965
7shi <7shi@live.jp>
2015-11-25 11:08:48 +0900

    関数テンプレートを関数オブジェクトで書き換え

--- parser.cpp
+++ parser.cpp
@@ -33,7 +33,20 @@ public:
     }
 };
 
-void parseTest(std::string (*p)(Source *), const char *s) {
+class Parser {
+protected:
+    virtual void check(Source *, char) const {}
+
+public:
+    virtual std::string operator()(Source *s) const {
+        char ch = s->peek();
+        check(s, ch);
+        s->next();
+        return std::string(1, ch);
+    };
+} anyChar;
+
+void parseTest(const Parser &p, const char *s) {
     Source src = s;
     try {
         std::cout << p(&src) << std::endl;
@@ -42,23 +55,32 @@ void parseTest(std::string (*p)(Source *), const char *s) {
     }
 }
 
-template<bool (*f)(char)> std::string satisfy(Source *s, const std::string &err) {
-    char x = s->peek();
-    if (!f(x)) throw s->ex("not " + err + ": '" + x + "'");
-    s->next();
-    return std::string(1, x);
-}
-template<bool (*f)(char)> std::string satisfy(Source *s) {
-    return satisfy<f>(s, "???");
-}
+class char1 : public Parser {
+    char ch;
 
-bool any(char) { return true; }
-std::string anyChar(Source *s) { return satisfy<any>(s, "anyChar"); }
+public:
+    char1(char ch) : ch(ch) {}
 
-template<char c> bool isChar(char ch) { return ch == c; }
-template<char c> std::string char1(Source *s) {
-    return satisfy< isChar<c> >(s, std::string("char '") + c + "'");
-}
+protected:
+    virtual void check(Source *s, char ch) const {
+        if (this->ch != ch) {
+            throw s->ex(std::string("char '") + this->ch + "': '" + ch + "'");
+        }
+    }
+};
+
+class satisfy : public Parser {
+    bool (*f)(char);
+    std::string err;
+
+public:
+    satisfy(bool (*f)(char), const std::string &err) : f(f), err(err) {}
+
+protected:
+    virtual void check(Source *s, char ch) const {
+        if (!f(ch)) throw s->ex("not " + err + ": '" + ch + "'");
+    }
+};
 
 bool isDigit   (char ch) { return std::isdigit(ch); }
 bool isUpper   (char ch) { return std::isupper(ch); }
@@ -67,31 +89,37 @@ bool isAlpha   (char ch) { return std::isalpha(ch); }
 bool isAlphaNum(char ch) { return isalpha(ch) || isdigit(ch); }
 bool isLetter  (char ch) { return isalpha(ch) || ch == '_';   }
 
-std::string digit   (Source *s) { return satisfy<isDigit   >(s, "digit"   ); }
-std::string upper   (Source *s) { return satisfy<isUpper   >(s, "upper"   ); }
-std::string lower   (Source *s) { return satisfy<isLower   >(s, "lower"   ); }
-std::string alpha   (Source *s) { return satisfy<isAlpha   >(s, "alpha"   ); }
-std::string alphaNum(Source *s) { return satisfy<isAlphaNum>(s, "alphaNum"); }
-std::string letter  (Source *s) { return satisfy<isLetter  >(s, "letter"  ); }
+satisfy digit   (isDigit   , "digit"   );
+satisfy upper   (isUpper   , "upper"   );
+satisfy lower   (isLower   , "lower"   );
+satisfy alpha   (isAlpha   , "alpha"   );
+satisfy alphaNum(isAlphaNum, "alphaNum");
+satisfy letter  (isLetter  , "letter"  );
 
-std::string test1(Source *s) {
-    std::string x1 = anyChar(s);
-    std::string x2 = anyChar(s);
-    return x1 + x2;
-}
+struct Test1 : public Parser {
+    virtual std::string operator()(Source *s) const {
+        std::string x1 = anyChar(s);
+        std::string x2 = anyChar(s);
+        return x1 + x2;
+    }
+} test1;
 
-std::string test2(Source *s) {
-    std::string x1 = test1(s);
-    std::string x2 = anyChar(s);
-    return x1 + x2;
-}
+struct Test2 : public Parser {
+    virtual std::string operator()(Source *s) const {
+        std::string x1 = test1(s);
+        std::string x2 = anyChar(s);
+        return x1 + x2;
+    }
+} test2;
 
-std::string test3(Source *s) {
-    std::string x1 = letter(s);
-    std::string x2 = digit(s);
-    std::string x3 = digit(s);
-    return x1 + x2 + x3;
-}
+struct Test3 : public Parser {
+    virtual std::string operator()(Source *s) const {
+        std::string x1 = letter(s);
+        std::string x2 = digit(s);
+        std::string x3 = digit(s);
+        return x1 + x2 + x3;
+    }
+} test3;
 
 int main() {
     parseTest(anyChar   , "abc" );
@@ -99,8 +127,8 @@ int main() {
     parseTest(test2     , "abc" );
     parseTest(test2     , "12"  );  // NG
     parseTest(test2     , "123" );
-    parseTest(char1<'a'>, "abc" );
-    parseTest(char1<'a'>, "123" );  // NG
+    parseTest(char1('a'), "abc" );
+    parseTest(char1('a'), "123" );  // NG
     parseTest(digit     , "abc" );  // NG
     parseTest(digit     , "123" );
     parseTest(letter    , "abc" );
