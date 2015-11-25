ff4d6b090f94f71dd37b87dd1fe4fb51da306860
7shi <7shi@live.jp>
2015-11-26 03:06:02 +0900

    privateを省略

--- cpp2-11.cpp
+++ cpp2-11.cpp
@@ -4,7 +4,6 @@
 #include <functional>
 
 class Source {
-private:
     const char *p;
     int line, col;
 public:
--- parser.cpp
+++ parser.cpp
@@ -3,7 +3,6 @@
 #include <string>
 
 class Source {
-private:
     const char *p;
     int line, col;
 public:
